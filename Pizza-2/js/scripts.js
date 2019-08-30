b388492e8869a0bd6fa0d7eec47207590bf96852
Student <example@example.com>
2019-08-30 11:56:39 -0700

    save my html and JavaScript experiments

--- Pizza-2/js/scripts.js
+++ Pizza-2/js/scripts.js
@@ -1,8 +1,8 @@
-function Pizza() {
+function Pizza(){
   this.size = size,
   this.sauce = sauce,
   this.toppings = toppings,
-  this.price= 0,
+  this.total= [this.size + this.sauce+ this.toppings];
 
 
 Pizza.prototype.orderPizza = function() {
@@ -13,15 +13,15 @@ Pizza.prototype.orderPizza = function() {
     return total;
 }
 
-
+console.log(size);
 
 
 $(document).ready(function() {
-  getInputs("size", 1);
+  getInputs("sz", 1);
 
-  getInputs("sauce", 1);
+  getInputs("sc", 1);
 
-  getInputs("toppings", 1);
+  getInputs("tps", 1);
 
 });
 
@@ -31,8 +31,15 @@ function getInputs(value, listCount){
 
     var inputNums = [];
     for(var i=1; i<=listCount; i++){
-      inputNums.push($("#item"+num+"-"+value).val().toUppercase(0));
-    };
+      inputNums.push(i);
+    }
+
+    var items=[];
+    inputNums.forEach(function(num){
+      items.push($("#item"+num+"-"+value).val().toUppercase(0));
+    });
+
+    items.add();
 
     items.forEach(function(item){
       $("#receipt"+value).append("<input>"+item+"</input>");
