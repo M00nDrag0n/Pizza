5c06cc59fc36a635397060e0c43b8fc692cd2322
Student <example@example.com>
2019-08-30 11:33:29 -0700

    save JS changes until I can figure this out

--- Pizza-2/js/scripts.js
+++ Pizza-2/js/scripts.js
@@ -1,21 +1,41 @@
 function Pizza() {
   this.size = size,
   this.sauce = sauce,
-  this.toppings = [],
+  this.toppings = toppings,
   this.price= 0,
-}
+
 
 Pizza.prototype.orderPizza = function() {
-  var 
+  var size= [];
+  var sauce = [];
+  var toppings = [];
+  var total = [size + sauce + toppings];
+    return total;
 }
 
 
 
 
+$(document).ready(function() {
+  getInputs("size", 1);
 
-$(document).ready(function()) {
-  $("#")
+  getInputs("sauce", 1);
 
+  getInputs("toppings", 1);
 
-  event.preventDefault();
 });
+
+function getInputs(value, listCount){
+  $("#form-" + value).submit(function(event){
+    event.preventDefault();
+
+    var inputNums = [];
+    for(var i=1; i<=listCount; i++){
+      inputNums.push($("#item"+num+"-"+value).val().toUppercase(0));
+    };
+
+    items.forEach(function(item){
+      $("#receipt"+value).append("<input>"+item+"</input>");
+    });
+    });
+}
